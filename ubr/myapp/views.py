a2cfd274e8a153a6ae81367209c820a2635faec3
Nafiz Ahmed Nafi <nafizahmednafi@gmail.com>
2025-12-22 23:20:30 +0600

    models,urls,views

--- ubr/myapp/views.py
+++ ubr/myapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+
+def signup(request):
+    """
+    Handle user registration using Django's built-in UserCreationForm.
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user in after successful registration
+            login(request, user)
+            messages.success(request, f'Account created successfully! Welcome, {user.username}!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def home(request):
+    """
+    Simple home page view.
+    """
+    return render(request, 'home.html')
