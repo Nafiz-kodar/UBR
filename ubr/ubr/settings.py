6856881d0d1f5b4b45bb159baa84c36942c9224c
Nafiz Ahmed Nafi <nafizahmednafi@gmail.com>
2025-12-25 21:02:10 +0600

    db added in settings

--- ubr/ubr/settings.py
+++ ubr/ubr/settings.py
@@ -74,8 +74,16 @@ WSGI_APPLICATION = 'ubr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'UBR',  # Or 'ubr_db' - whatever your database name is
+        'USER': 'ubr_user',
+        'PASSWORD': 'ubr_nsm123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
